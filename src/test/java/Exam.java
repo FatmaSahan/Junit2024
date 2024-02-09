f34aacf5992e3e697d1bdfc614a38ff97ea9cc52
fatma <ffsbm3842@gmail.com>
2024-02-09 23:58:33 +0000

    9 february

--- src/test/java/Exam.java
+++ src/test/java/Exam.java
@@ -1,6 +1,12 @@
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Exam {
     // 1) Bir class oluşturun: YoutubeAssertions
@@ -9,8 +15,18 @@ public class Exam {
     @Before
     public void setUp(){
         driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
     }
+    @Test
+    public void test01(){
+        driver.get("https://www.youtube.com");
+
+
+
+    }
+
     //            3) Aşağıdaki adları kullanarak 3 test metodu oluşturun ve gerekli testleri yapin
     //○ titleTest
     //○ imageTest
